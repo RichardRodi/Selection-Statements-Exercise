e6a28f4f655751b4dd30ec7235a738b0c6681d00
RichardRodi <RichardmRodi@gmail.com>
2024-02-20 12:21:19 -0500

    Created the Number Guessing Game

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -1,10 +1,46 @@
-﻿namespace SelectionStatementExercise
+﻿using System.ComponentModel.Design;
+using System.Diagnostics.Metrics;
+using System.Runtime.InteropServices;
+
+namespace SelectionStatementExercise
 {
     public class Program
     {
         static void Main(string[] args)
+
         {
-            Console.WriteLine("Hello, World!");
+            var r = new Random();
+            var favNumber = r.Next(1, 100);
+            var counter = 0;
+            int userInput;
+            
+
+                do
+                {
+                
+                Console.WriteLine("Guess a Number From 1-100");
+
+                userInput = int.Parse(Console.ReadLine());
+
+                if (userInput < favNumber)
+                {
+                    Console.WriteLine($"{userInput} is Too Low!");
+                }
+                else if (userInput > favNumber)
+                {
+
+                    Console.WriteLine($"{userInput} is Too High!");
+                }
+                else  
+                {
+                    Console.WriteLine("You Guessed It");
+
+                }
+                counter++;
+
+                    Console.WriteLine($"You guessed {counter} times");
+
+            }   while (userInput != favNumber); 
         }
     }
 }
